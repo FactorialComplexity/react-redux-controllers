61d81c317b4c62e9e2f4f0b11153f067e18af9c2
Vitaliy Ivanov <wicharek@gmail.com>
2017-05-15 00:22:43 +0300

    Action: added support for initializers and sub-reducers

--- src/utils/Action.js
+++ src/utils/Action.js
@@ -63,10 +63,7 @@ export default class Action {
             handler = arguments[0];
         }
 
-        return {
-            type: this.type(stage),
-            handler
-        }
+        return Object.assign(handler, { type: this.type(stage) });
     }
 
     onStarted(handler) {
@@ -80,23 +77,52 @@ export default class Action {
     onError(handler) {
         return this.on(Action.Stage.ERROR, handler);
     }
+    
+    static initial(functionOrState) {
+        if (typeof functionOrState === "function") {
+            return Object.assign(functionOrState, { initial: true });
+        } else {
+            return Object.assign(() => functionOrState, { initial: true });
+        }
+    }
 
-    static createReducer(...actionHandlers) {
+    static createReducer(...args) {
+        var actionHandlers = args, key;
+        if (typeof args[0] === "string") {
+            key = args[0];
+            actionHandlers = args.slice(1);
+        }
+        
+        actionHandlers = actionHandlers.filter((h) => !h.initial);
+        const initializer = actionHandlers.filter((h) => h.initial)[0];
+        
         return (state, action, fullState) => {
+            let parentState;
+            if (key) {
+                parentState = state;
+                state = state[key];
+            }
+            
             if (!state) {
-                return { };
+                state = initializer ? initializer() : { };
+            } else {
+                actionHandlers.forEach((actionHandler) => {
+                    if (action.type === actionHandler.type || actionHandler.type === undefined) {
+                        const newState = actionHandler(state,
+                            actionHandler.type === undefined ? action : action.payload,
+                            fullState);
+                        if (newState !== undefined) {
+                            state = newState;
+                        }
+                    }
+                });
             }
 
-            actionHandlers.forEach((actionHandler) => {
-                if (action.type === actionHandler.type) {
-                    const newState = actionHandler.handler(state, action.payload, fullState);
-                    if (newState !== undefined) {
-                        state = newState;
-                    }
-                }
-            });
-
-            return state;
+            if (parentState) {
+                return Object.assign({ }, parentState, { [key]: state });
+            } else {
+                return state;
+            }
         };
     }
 }
