b01c74997102279b0f259ed478028fe910af9a61
Vitaliy Ivanov <wicharek@gmail.com>
2017-07-18 18:26:10 +0300

    Added basic unit test for Container

---
+++ test/Container.spec.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, Container } from '../src'
+import { NoOpController } from './helpers/controllers.js'
+
+describe('Container', () => {
+  it('maps dispatches and selectors', () => {
+    class MyController extends NoOpController {
+      $foo(state) {
+        return 'bar'
+      }
+      
+      dispatchBar() { dispatchBar() }
+    }
+    
+    const controller = new MyController()
+    const store = createStore(combineReducers({ controller }))
+    
+    const TestComponent = () => (<div/>)
+    const Wrapper = Container(TestComponent, 'controller.*')
+    
+    const wrapper = mount(<Wrapper />, { context: { store } })
+    
+    // console.log(wrapper.debug())
+    
+    expect(wrapper.find(TestComponent).length).toBe(1)
+    
+    const wrapped = wrapper.find(TestComponent)
+    
+    expect(wrapped.prop('foo')).toBe('bar')
+    expect(typeof wrapped.prop('bar') === 'function')
+  })
+})
