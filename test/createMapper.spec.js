1f1ba3cffb40cb5aa85681e90e2eff2090eef37a
Vitaliy Ivanov <wicharek@gmail.com>
2017-07-25 12:47:50 +0300

    Implemented support for processing Controller selectors, when Controller is located deeper then mapped state. Fixed dispatch mapping. Fixed caching. Better caching tests.

--- test/createMapper.spec.js
+++ test/createMapper.spec.js
@@ -5,33 +5,71 @@ import { createStore, combineReducers } from '../src'
 import createMapper from '../src/mapper/createMapper'
 import normalizeMappings from '../src/mapper/normalizeMappings'
 import shallowEqual from '../src/utils/shallowEqual'
-import { NoOpController } from './helpers/controllers.js'
+import Controller from '../src/Controller'
 
 describe('createMapper', () => {
   const dispatchBar = jest.fn()
-  class MyController extends NoOpController {
-    $foo (state) {
-      return 'bar'
+  class MyController extends Controller {
+    constructor () {
+      super()
+
+      this.createAction('bar')
     }
 
-    dispatchBar () { dispatchBar() }
+    $foo (state) {
+      return !this.$$(state)._barDispatched ? 'bar' : 'barbar'
+    }
+
+    dispatchBar () {
+      this.dispatchAction('bar')
+      dispatchBar()
+    }
+
+    reducer () {
+      const { bar } = this.actions
+      return this.createReducer(
+        bar.on((state) => Object.assign({}, state, { _barDispatched: true }))
+      )
+    }
   }
 
   let controller
   let store
+  let deepController
+  let deepStore
 
   beforeEach(() => {
     dispatchBar.mockClear()
 
     controller = new MyController()
-    const $foo = controller.$foo.bind(this)
+    const $foo = controller.$foo.bind(controller)
     controller.$foo = jest.fn((...args) => $foo(...args))
 
     store = createStore(combineReducers({ controller }), {
       controller: {
         preloaded: 'preloaded value',
         another: 'another preloaded value',
-        _doNotMap: 'value'
+        _barDispatched: false
+      }
+    })
+
+    deepController = new MyController()
+    const $deepFoo = deepController.$foo.bind(deepController)
+    deepController.$foo = jest.fn((...args) => $deepFoo(...args))
+
+    deepStore = createStore(combineReducers({
+      key1: combineReducers({
+        key2: combineReducers({ deepController })
+      })
+    }), {
+      key1: {
+        key2: {
+          deepController: {
+            preloaded: 'preloaded value',
+            another: 'another preloaded value',
+            _doNotMap: 'value'
+          }
+        }
       }
     })
   })
@@ -179,5 +217,39 @@ describe('createMapper', () => {
 
     expect(controller.$foo).not.toHaveBeenCalled()
     expect(shallowEqual(result1, result2)).toBe(true)
+    controller.$foo.mockClear()
+
+    result2.bar()
+
+    const result3 = mapper(store.getState())
+    expect(controller.$foo).toHaveBeenCalled()
+    expect(shallowEqual(result2, result3)).toBe(false)
+  })
+
+  it('supports deep mapping of state, calling selectors when required', () => {
+    const mapper = createMapper(deepStore.getController,
+      normalizeMappings(['key1.*', 'key1.key2.deepController.bar']), 'createMapper.spec')
+
+    const result = mapper(deepStore.getState())
+
+    expect(Object.keys(result.key2.deepController).length).toBe(3)
+
+    expect(result.key2.deepController.foo).toBe('bar')
+    expect(result.key2.deepController.preloaded).toBe('preloaded value')
+    expect(result.key2.deepController.another).toBe('another preloaded value')
+    deepController.$foo.mockClear()
+
+    const result2 = mapper(deepStore.getState())
+
+    expect(deepController.$foo).not.toHaveBeenCalled()
+    expect(shallowEqual(result, result2)).toBe(true)
+    deepController.$foo.mockClear()
+
+    result2.bar()
+
+    const result3 = mapper(deepStore.getState())
+
+    expect(deepController.$foo).toHaveBeenCalled()
+    expect(shallowEqual(result2, result3)).toBe(false)
   })
 })
